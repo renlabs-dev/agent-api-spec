67d17481717ad399140568a63899e8ca7a25063d
Cipriano <gabrielfcipriano@gmail.com>
2024-05-14 07:46:36 +0200

    chore: string literal on tag names

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,10 @@ declare const TAG_KEY: unique symbol;
 
 export type BrandTag<T> = { readonly [TAG_KEY]: T };
 
+
+// Allows ONLY string literals
+type StringLiteral<T extends string> = string extends T ? never : T;
+
 /**
  * Ad-hoc specilization of a type, for extra type info that can't be expressed
  * in TypeScript type system.
@@ -11,7 +15,9 @@ export type BrandTag<T> = { readonly [TAG_KEY]: T };
  * - `Value`: type of the actual value
  * - `P`: ad-hoc param to differentiate generic branded types
  */
-export type Brand<Tag, Value, P = null> =
-    P extends null
-        ? BrandTag<Tag> & Value
-        : BrandTag<[Tag, P]> & Value;
+export type Brand<Tag extends string, Value, P = null> =
+    Tag extends StringLiteral<Tag>
+        ? P extends null
+            ? BrandTag<Tag> & Value
+            : BrandTag<[Tag, P]> & Value
+        : never;
